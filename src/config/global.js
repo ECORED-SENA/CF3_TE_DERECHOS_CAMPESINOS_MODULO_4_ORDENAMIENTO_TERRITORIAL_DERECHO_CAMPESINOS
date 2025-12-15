c4bf654096266870dd0ae3e0e73b212935390e6a
Leyson Castaño <leysofcp@hotmail.com>
2025-12-15 12:39:01 -0500

    Se adjunta CFA y fuentes

--- src/config/global.js
+++ src/config/global.js
@@ -157,7 +157,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/212500_M4_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -16,8 +16,8 @@
     
     figure.mb-5
       .video.mb-2
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/aG9iZdliynU?si=Prn3XDq8yMwFQxds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
     
     separador
     
